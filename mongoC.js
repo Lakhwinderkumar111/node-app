0ae6c96f2221790cc361a294204deb2b695e7e70
Lakhwinderkumar111 <jk910071@gmail.com>
2025-09-09 16:55:36 +0530

    aws_deployment14

--- mongoC.js
+++ mongoC.js
@@ -1,7 +1,7 @@
 import { MongoClient } from "mongodb";
 
 const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
-const connectionString = `mongodb+srv://integrationninjas:${password}@devcluster.hu5pjmi.mongodb.net/?retryWrites=true&w=majority`; // cluster url
+const connectionString = `mongodb+srv://jk910071_db_user:${ jk910071_db_user}@cluster1app.ou7tjj1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1app`; // cluster url
 const client = new MongoClient(connectionString);
 let conn;
 try {
